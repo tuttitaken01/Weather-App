986c38dafc882390bad7b9d5c18b28bcf2ececcc
Valentina <vh010200@gmail.com>
2023-01-30 18:26:59 +0100

    edits that need change

--- src/components/search.js
+++ src/components/search.js
@@ -1,11 +1,33 @@
 import { useState } from 'react';
 import { AsyncPaginate } from 'react-select-async-paginate';
-import { geoApiOptions, GEO_API_URL } from 
+import { geoApiOptions, GEO_API_URL } from './api';
 import IconButton from '@mui/material/IconButton';
 import SearchIcon from '@mui/icons-material/Search';
 
 const Search = ({ city }) => {
     const [searchCity, setCity] = useState('');
+
+    const loading = (input) => {
+        return fetch(
+            `${GEO_API_URL}/cities?limit=5&namePrefix=${input}`, geoApiOptions
+        )
+        .then(res => res.json())
+        .then(res => {
+            return {
+                options: res.data.map(city => {
+                    return {
+                        value: `${city.latitude} ${city.longitude}`,
+                        label: `${city.name}, ${city.countryCode}`,
+                    }
+                })
+            }
+        })
+    }
+
+    const handleOnChange = (search) => {
+        setCity(search);
+        city(search);
+    }
 
     const handleSubmit = (e) => {
         e.preventDefault();
